2cadbd66e84b702940c46a83b5c4a2f1f504d9d2
Taylor Larsen <taylor@tangiblematerials.com>
2025-08-26 19:15:44 -0400

    add post model

---
+++ app/avo/resources/post.rb
@@ -0,0 +1,12 @@
+class Avo::Resources::Post < Avo::BaseResource
+  # self.includes = []
+  # self.attachments = []
+  # self.search = {
+  #   query: -> { query.ransack(id_eq: q, m: "or").result(distinct: false) }
+  # }
+
+  def fields
+    field :id, as: :id
+    field :title, as: :text
+  end
+end
